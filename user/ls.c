37413fa484fbcd39574596874942443c9246c46c
xiuxiubiu <xiuxiubiu@gmail.com>
2024-03-14 16:02:27 +0800

    feat: init lab

--- user/ls.c
+++ user/ls.c
@@ -1,8 +1,8 @@
-#include "kernel/types.h"
-#include "kernel/stat.h"
-#include "user/user.h"
-#include "kernel/fs.h"
-#include "kernel/fcntl.h"
+#include "../kernel/types.h"
+#include "../kernel/stat.h"
+#include "user.h"
+#include "../kernel/fs.h"
+#include "../kernel/fcntl.h"
 
 char*
 fmtname(char *path)
