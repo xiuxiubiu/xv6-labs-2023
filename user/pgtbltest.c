402ff4c922dd097806a382cc6969bdcea524683f
xiuxiubiu <xiuxiubiu@gmail.com>
2024-04-08 10:42:39 +0800

    feat: initlize

--- user/pgtbltest.c
+++ user/pgtbltest.c
@@ -1,8 +1,8 @@
-#include "kernel/param.h"
-#include "kernel/fcntl.h"
-#include "kernel/types.h"
-#include "kernel/riscv.h"
-#include "user/user.h"
+#include "../kernel/param.h"
+#include "../kernel/fcntl.h"
+#include "../kernel/types.h"
+#include "../kernel/riscv.h"
+#include "user.h"
 
 void ugetpid_test();
 void pgaccess_test();
--- user/wc.c
+++ user/wc.c
@@ -1,7 +1,7 @@
-#include "kernel/types.h"
-#include "kernel/stat.h"
-#include "kernel/fcntl.h"
-#include "user/user.h"
+#include "../kernel/types.h"
+#include "../kernel/stat.h"
+#include "../kernel/fcntl.h"
+#include "user.h"
 
 char buf[512];
 
